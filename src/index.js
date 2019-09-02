f08b563a2d027f79acd3b979843f12bb75e906b1
AJ <amarchitelli1993@gmail.com>
2019-09-01 20:05:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+
+  console.log('%c HI', 'color: firebrick')
+
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(imageHash => imageHash.message.forEach(imageUrl => {
+      
+      let newImage = document.createElement("img");
+      newImage.setAttribute('src', imageUrl);
+      
+      let dogDiv = document.querySelector('#dog-image-container')
+      dogDiv.appendChild(newImage)
+  }))
+
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(response => Object.keys(response.message).forEach(breed => {
+
+      let newBreed = document.createElement('li');
+
+      newBreed.innerText = breed;
+
+      let breedList = document.querySelector('#dog-breeds');
+      breedList.appendChild(newBreed);
+
+    }))
+
+    let breedUl = document.querySelector('#dog-breeds');
+    breedUl.addEventListener('click', function(event) {
+      if(event.target.tagName === 'LI') {
+        event.target.style.color = "#"+((1<<24)*Math.random()|0).toString(16)
+      }
+    })
+
+    let dropDown = document.querySelector('#breed-dropdown');
+      dropDown.addEventListener('change', function(event) {
+        if(event.target.value === 'a') {
+          debugger
+          console.log(breedUl.innerText)
+        }
+      })
+      
+
+})
